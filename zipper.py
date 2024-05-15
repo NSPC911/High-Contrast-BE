f4d51254c8c0e312ec92cd07542915d14da9340a
NSPC911 <notsoprocoder911@gmail.com>
2024-05-15 16:00:51 +0800

    Updated for new Touch UI Checkbox background remains unfixed, I'm not sure where it is at

--- zipper.py
+++ zipper.py
@@ -2,6 +2,14 @@ import os
 import zipfile
 import time
 
+if str(os.getcwd()).endswith("system32"):
+    # This has to be in every script to prevent FileNotFoundError
+    # Because for some reason, it runs it at C:\Windows\System32
+    # Yeah, it is stupid, but I can't put these lines in custom_functions
+    # Because that still brings up an error
+
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 output_dir = '.'
 repo_path = os.getcwd()
 output_path = os.path.join(output_dir, f"pack.mcpack")
@@ -22,10 +30,10 @@ with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
             file_path = os.path.join(root, file)
             file_path_length = len(file_path)
             try:
-                print(f"\rArchiving: {file_path}{' ' * (os.get_terminal_size().columns - file_path_length - 12)}", end='', flush=True)
+                print(f"\rArchiving: {str(file_path)[len(str(os.getcwd())):]}{' ' * (os.get_terminal_size().columns - len(str(file_path)[len(str(os.getcwd())):]) - 12)}", end='', flush=True)
             except OSError:
                 print(f"\rArchiving: {file_path}", end='', flush=True)
             zipf.write(file_path, os.path.relpath(file_path, repo_path))
 elapsed_time = time.time() - start_time
-print(f"\nRepository zipped successfully at {output_path}")
-print(f"Elapsed Time: {elapsed_time:.2f} seconds")
+print(f"\rRepository zipped successfully at {output_path}{' ' * os.get_terminal_size().columns}",flush=True)
+input(f"Elapsed Time: {elapsed_time:.2f} seconds")
